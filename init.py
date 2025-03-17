c3d91e23db5f29a542302f142ee5c0576e82d60c
Pingvin09 <simonscat1214@gmail.com>
2025-03-17 15:55:29 -0300

    add country

--- init.py
+++ init.py
@@ -1,18 +1,43 @@
+from itertools import count
+
 import pygame as pg
 import sys
-
 pg.init()
-
+print("call of war\nWW3\n\n\n\n")
 screen = pg.display.set_mode((800,800))
 pg.display.set_caption("Call of war, world war three")
 map_image = pg.image.load("Map_image.jpg")
 
+country = [
+    {"x":530,
+     "y": 230,
+     "name": "USSR",
+     "arms": 99999,
+     "slawes": 159,
+     "people": 30987669,
+     "people+": 480000},
+    {"x": 158,
+     "y": 242,
+     "name": "USA",
+     "arms": 99999,
+     "slawes": 159,
+     "people": 30987669,
+     "people+": 480000}
+]
+
+pg.font.init()
+font = pg.font.SysFont(None, 24)
+
 running = True
 map_image = pg.transform.scale(map_image,(800,800))
 while running:
+    screen.blit(map_image, (0, 0))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-    screen.blit(map_image,(0,0))
-    pg.draw.circle(screen, (180, 0, 0), (650, 350), 2)
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            x, y = event.pos
+            print(f"x {x}, y {y}")
+    for country1 in country:
+        pg.draw.circle(screen, (255, 255, 255), (country1["x"], country1["y"]), 5)
     pg.display.flip()
